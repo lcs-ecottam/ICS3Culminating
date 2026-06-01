806c7701189f4ae965c4fef83c10fa1e94709736
Ella Cottam <ecottam@lcs.on.ca>
2026-06-01 14:50:18 -0400

    Initial Commit

---
+++ ICS3Culminating/ICS3CulminatingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS3CulminatingApp.swift
+//  ICS3Culminating
+//
+//  Created by Ella Seville-Cottam on 2026-06-01.
+//
+
+import SwiftUI
+
+@main
+struct ICS3CulminatingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
